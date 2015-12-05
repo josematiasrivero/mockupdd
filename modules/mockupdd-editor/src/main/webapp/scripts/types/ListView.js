5511b66fa3796fc47cafeb9c924018d6625f41dc
Jerónimo Spuri Murguía <jspurim@gmail.com>
2015-12-05 17:06:19 -0300

    List element addition working.

--- modules/mockupdd-editor/src/main/webapp/scripts/types/ListView.js
+++ modules/mockupdd-editor/src/main/webapp/scripts/types/ListView.js
@@ -4,7 +4,7 @@ var ListView = TypeView.extend({
 		this._itemType = itemType;
 		this._items = [];
 		for(item in this._value){
-			this._items.push(new itemType.getListItemView()(item));
+			this._items.push(itemType.getListItemView(this._value[item]));
 		}
 	},
 	
@@ -12,28 +12,40 @@ var ListView = TypeView.extend({
 		return value;
 	},
 	
+	getValue : function(){
+		var ret = [];
+		for(i in this._items){
+			ret.push(this._items[i].getValue());
+		}
+		return ret;
+	},
+	
 	getDom : function() {
-		this._wrapper = $("<div class='input-group'>");
-		this._list = $("<ul class='list-group'>");
+		this._wrapper = $("<div class='row'>");
+		var colLeft = $("<div class='col-md-8'>");
+		var colRight = $("<div class='col-md-4 list-button-wrapper'>");
+		this._wrapper.append(colLeft);
+		this._wrapper.append(colRight);
+		this._list = $("<ul class='list-group form-list'>");
 		
 
 		for(item in this._items){
-			this._list.append(this._itemType.getListItemView(item));
+			this._list.append(this._items[item].getDom());
 		}
 		
 		var addBtn = $("<button class='btn btn-primary'>New</button>");
 		var delBtn = $("<button class='btn btn-danger'>Delete</button>");
 		
-		this._wrapper.append(this._list);
-		this._wrapper.append(addBtn);
-		this._wrapper.append(delBtn);
+		colLeft.append(this._list);
+		colRight.append(addBtn);
+		colRight.append(delBtn);
 		
 		addBtn.click($.proxy(function(){
 			var itemType = window[this._itemType.getTypeName()];
 			var item = new itemType();
 			var listItemView = this._itemType.getListItemView(item);
 			this._items.push(listItemView);
-			this._list.append(listItemView.getDom);
+			this._list.append(listItemView.getDom());
 		}, this));
 		return this._wrapper;
 		
